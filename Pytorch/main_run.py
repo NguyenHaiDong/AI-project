49e688c6e09207f60c9df695aa302403bb9dbc30
NguyenHaiDong <haidong19022010@gmail.com>
2026-09-03 22:14:59 +0700

    Increase batch size from 256 to 512 in training

--- Pytorch/main_run.py
+++ Pytorch/main_run.py
@@ -33,7 +33,7 @@ data = MLP.extract(device, train_data, train_label, [128, 64, 64, 64], list_func
 gradient_descent = MLP.Momentum(device, data)
 neural_network = MLP.Neural_Network(device, data, gradient_descent)
 
-it = neural_network.fit(device, batch_size = 256, max_it = 150)
+it = neural_network.fit(device, batch_size = 512, max_it = 150)
 pred = neural_network.predict(test_data)
 
 algorithm.close_clock_and_show_time(device, start_time)
